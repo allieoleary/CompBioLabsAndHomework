c384b7e58eeb3e5620ce5f8cfb0acb6ef2d29ac6
allieoleary <alol2184@colorado.edu>
2020-04-10 13:50:43 -0600

    Finished lab 11 to now include ggplots

--- Labs/Lab11/Lab11.R
+++ Labs/Lab11/Lab11.R
@@ -18,17 +18,60 @@ MainData <- MainData[-NAPosition, ] # removing the NA row
 nrow(MainData) # ensuring that I have one less row than before
 
 #Q5
-BinomFamilyMean <- MainData %>% group_by(Binomial, Family) %>% summarise(mean(WoodDensity))
-colnames(BinomFamilyMean)[3] <- "WoodDensityMean"
+BinomFamilyMean <- MainData %>% group_by(Binomial, Family) %>% summarise(mean(WoodDensity)) # piping so that we only have Binomial, family and their wood density means in a graph
+colnames(BinomFamilyMean)[3] <- "WoodDensityMean" # renaming the wood density mean column name
 
-#6
+#Q6
 #a
-FamilyMean <- BinomFamilyMean %>% group_by(Family) %>% summarise(mean(WoodDensityMean))
-colnames(FamilyMean)[2] <- "WoodDensityMeanFam"
+FamilyMean <- BinomFamilyMean %>% group_by(Family) %>% summarise(mean(WoodDensityMean)) # piping so that we only have family and wood density means
+colnames(FamilyMean)[2] <- "WoodDensityMeanFam" # renaming the wood density mean column name 
 #b
-AscendingFamilyMean <- FamilyMean %>% arrange(WoodDensityMeanFam)
+AscendingFamilyMean <- FamilyMean %>% arrange(WoodDensityMeanFam) # sorting our results from 6a in order of lowest wood density to highest 
 #c
-LastEight <- nrow(AscendingFamilyMean)
-LargestEight <- AscendingFamilyMean$Family[(LastEight-7):LastEight]
-SmallestEight <- AscendingFamilyMean$Family[1:8]
+LastEight <- nrow(AscendingFamilyMean) # creating a variable to help us section out the largest eight means
+LargestEight <- AscendingFamilyMean$Family[(LastEight-7):LastEight] # creating a varable that shows us which 8 familes have the largest densities 
+View(LargestEight)
+SmallestEight <- AscendingFamilyMean$Family[1:8] # creating a variable that shows us which families have the smallest densities
+View(SmallestEight)
+
+
+#### Part III: Plotting ####
+#Q7
+library("ggplot2") 
+#GraphOne
+EightMostDense <- BinomFamilyMean[ BinomFamilyMean$Family %in% LargestEight, ] # subsetting data to work with it and combine data from question 5 and 6
+Lp <- ggplot(EightMostDense) + 
+  geom_boxplot(aes(y = WoodDensityMean)) # creating the main ggplot for the eight most dense species 
+Lp <- Lp + theme(
+  axis.ticks.x = element_blank(),
+  axis.text.x = element_blank()) # removing x axis ticks and numbers 
+Lp <- Lp + facet_wrap( ~ Family, scales="fixed", nrow = 2) # editing how the families apear: separate, one scale, two rows 
+Lp <- Lp + ylab("Density (g/cm^3)") + ggtitle("Mean Wood Density of Species by Family (Eight Largest)") # adding labels 
+Lp 
+#GraphTwo
+EightLeastDense <- BinomFamilyMean[ BinomFamilyMean$Family %in% SmallestEight, ] # subsetting data to work with it and combine data from question 5 and 6
+Sp <- ggplot(EightLeastDense) + 
+  geom_boxplot(aes(y = WoodDensityMean)) # creating the main ggplot for the eight least dense species 
+Sp <- Sp + theme(
+  axis.ticks.x = element_blank(),
+  axis.text.x = element_blank()) # removing x axis ticks and numbers 
+Sp <- Sp + facet_wrap( ~ Family, scales="fixed", nrow = 2) # editing how the families apear: separate, one scale, two rows 
+Sp <- Sp + ylab("Density (g/cm^3)") + ggtitle("Mean Wood Density of Species by Family (Eight Smallest)") # adding labels
+Sp 
+
+#Q8
+#GraphOne
+LpNew <- ggplot(EightMostDense) + 
+  geom_boxplot(aes(x = Family, y = WoodDensityMean)) # creating the main ggplot for the eight most dense species 
+LpNew <- LpNew + coord_flip() # flipping the coordinates
+LpNew <- LpNew + ylim(c(0,1.2)) # changing the limits on the x axis (or the y, as that is what R is thinking of it as)
+LpNew <- LpNew + xlab("Families")  + ylab("Density (g/cm^3)") + ggtitle("Mean Wood Density of Species by Family (Eight Largest)") # adding labels 
+LpNew
+#GraphTwo
+SpNew <- ggplot(EightLeastDense) + 
+  geom_boxplot(aes(x = Family, y = WoodDensityMean)) # creating the main ggplot for the eight most dense species 
+SpNew <- SpNew + coord_flip() # flipping the coordinates
+SpNew <- SpNew + ylim(c(0,1.2)) # changing the limits on the x axis (or the y, as that is what R is thinking of it as)
+SpNew <- SpNew + xlab("Families")  + ylab("Density (g/cm^3)") + ggtitle("Mean Wood Density of Species by Family (Eight Smallest)") # adding labels 
+SpNew
 
